8d2596dc525d71366c02e858a770556b9ee51016
zxh <zhangxiuhong@zyxtravel.com>
2018-08-04 22:05:45 +0800

    ch04: test

--- test.sh
+++ test.sh
@@ -5,12 +5,11 @@ set -ex
 cd code/go
 export GOPATH=$PWD
 go install luago
-luac ../lua/ch02/hello_world.lua
-bin/luago luac.out | grep LOADK
+bin/luago | grep hello
 
 # test java
 cd ../java
 gradle build
-java -cp build/classes/java/main com.github.zxh0.luago.Main ../go/luac.out | grep LOADK
+java -cp build/classes/java/main com.github.zxh0.luago.Main | grep hello
 
 echo OK
