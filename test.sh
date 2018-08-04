d5ce810253ffffa4302a43b173d78412fb6f29e3
zxh <zhangxiuhong@zyxtravel.com>
2018-08-04 23:09:34 +0800

    ch12: test

--- test.sh
+++ test.sh
@@ -5,12 +5,12 @@ set -ex
 cd code/go
 export GOPATH=$PWD
 go install luago
-luac ../lua/ch11/vector2.lua
-./bin/luago luac.out | tr -d '\n' | grep -F -q "[1, 2][3, 4][2, 4][3, 6]5falsetrue[3, 6]"
+luac ../lua/ch12/test.lua
+./bin/luago luac.out | grep "a\t1"
 
 # test java
 cd ../java
 gradle build
-java -cp build/classes/java/main com.github.zxh0.luago.Main ../go/luac.out | tr -d '\n' | grep -F -q "[1, 2][3, 4][2, 4][3, 6]5.0falsetrue[3, 6]"
+java -cp build/classes/java/main com.github.zxh0.luago.Main ../go/luac.out | grep "a\t1"
 
 echo OK
