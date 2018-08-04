555d0a0811f9b15067855cf45da4c64b65314dee
zxh <zhangxiuhong@zyxtravel.com>
2018-08-04 22:09:37 +0800

    ch05: test

--- test.sh
+++ test.sh
@@ -5,11 +5,11 @@ set -ex
 cd code/go
 export GOPATH=$PWD
 go install luago
-bin/luago | grep hello
+bin/luago | grep "2.0"
 
 # test java
 cd ../java
 gradle build
-java -cp build/classes/java/main com.github.zxh0.luago.Main | grep hello
+java -cp build/classes/java/main com.github.zxh0.luago.Main | grep "2.0"
 
 echo OK
