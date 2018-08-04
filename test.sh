e203e3e38ce7b496288298d9557b918a4012ab3f
zxh <zhangxiuhong@zyxtravel.com>
2018-08-04 23:43:52 +0800

    ch21: test

--- test.sh
+++ test.sh
@@ -5,7 +5,6 @@ set -ex
 cd code/go
 export GOPATH=$PWD
 go install luago
-cd ../lua/ch20
-../../go/bin/luago test.lua | tr -d '\n' | grep -q "foobar"
+./bin/luago ../lua/ch21/test.lua | tr -d '\n,' | grep -q "bcacbacabacbbacabc" 
 
 echo OK
