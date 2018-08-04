4079d02a075355ea89f950a9b57a9356675605eb
zxh <zhangxiuhong@zyxtravel.com>
2018-08-04 23:39:27 +0800

    ch19: test

--- test.sh
+++ test.sh
@@ -5,11 +5,6 @@ set -ex
 cd code/go
 export GOPATH=$PWD
 go install luago
-./bin/luago ../lua/ch02/hello_world.lua | grep "Hello, World!"
-
-# test java
-cd ../java
-gradle build
-java -cp build/classes/java/main com.github.zxh0.luago.Main ../lua/ch02/hello_world.lua | grep "Hello, World!"
+./bin/luago ../lua/ch19/examples.lua | grep "2018"
 
 echo OK
