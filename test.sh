0504f4587c4cc621782a9570971885714a6394d8
zxh <zhangxiuhong@zyxtravel.com>
2018-08-04 23:20:24 +0800

    ch15: test

--- test.sh
+++ test.sh
@@ -5,11 +5,9 @@ set -ex
 cd code/go
 export GOPATH=$PWD
 go install luago
-./bin/luago ../lua/ch02/hello_world.lua | grep EOF
 
 # test java
 cd ../java
 gradle build
-java -cp build/classes/java/main com.github.zxh0.luago.Main ../lua/ch02/hello_world.lua | grep EOF
 
 echo OK
